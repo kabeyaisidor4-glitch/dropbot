511d2360d5f6e75a0091503dd065cd4d758f13ad
kabeyaisidor4-glitch <kabeyaisidor4@gmail.com>
2026-05-04 14:22:03 +0200

    Create Shop model file

---
+++ models/Shop.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const shopSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, default: '' },
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    logo: { type: String, default: '' },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Shop', shopSchema);
